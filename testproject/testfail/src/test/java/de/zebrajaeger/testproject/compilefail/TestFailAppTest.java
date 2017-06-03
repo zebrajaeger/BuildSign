52980bfa3e71543ae71d77900a08b0d8cebe31bc
Lars <L@rs-Brandt.de>
2017-06-03 22:41:20 +0200

    add more tests

--- testproject/testfail/src/test/java/de/zebrajaeger/testproject/compilefail/TestFailAppTest.java
+++ testproject/testfail/src/test/java/de/zebrajaeger/testproject/compilefail/TestFailAppTest.java
@@ -9,7 +9,57 @@ import org.junit.Test;
 public class TestFailAppTest {
 
     @Test
-    public void foo() {
+    public void success_1() {
+
+    }
+
+    @Test
+    public void success_2() {
+
+    }
+
+    @Test
+    public void success_3() {
+
+    }
+
+    @Test
+    public void success_4() {
+
+    }
+
+    @Test
+    public void success_5() {
+
+    }
+
+    @Test
+    public void fail_1() {
         Assert.assertTrue(false);
+    }
+
+    @Test
+    public void success_6() {
+
+    }
+
+    @Test
+    public void success_7() {
+
+    }
+
+    @Test
+    public void success_8() {
+
+    }
+
+    @Test
+    public void success_9() {
+
+    }
+
+    @Test
+    public void success_10() {
+
     }
 }
